1c0b23aa5d7eb749343795f157e1298dc2e95574
fatihsenyigit <fsenyigit8@gmail.com>
2025-03-09 21:34:37 +0700

    main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,43 @@
+import { Container } from "@mui/material";
+import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-
-
-
-const Main = () => {
-  return (
-    <div>Main</div>
-  )
+interface ITodoType {
+  task: string;
+  isDone: boolean;
+  id: string | number;
+  todo?: string;
 }
 
-export default Main
+const url = "https://67cd5c91dd7651e464ee2794.mockapi.io/todos";
+
+const Main = () => {
+  // api dan gelen verilerle dolacagi icin
+  // objeler icin veri tanimlama yaparken en iyi interface ve interface lerde best practice icin isim verilirken I ile baslanir.
+  // const [todos, setTodos] = useState([] as ITodoType[]) birinci yol
+  // const [todos, setTodos] = useState<Array<ITodoType>>([]) ikinci yol
+  const [todos, setTodos] = useState<ITodoType[]>([]); //yaygin olan bu
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios<ITodoType[]>(url);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos()
+  }, [])
+  
+
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+};
+
+export default Main;
